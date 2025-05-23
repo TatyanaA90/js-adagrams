2cbec234a2630387bf14ea869a477318737b710f
Tatyana Ageyeva <tatyana.ageyeva.v@gmail.com>
2025-05-22 17:41:25 -0700

    computeCumulativeWeights other solution  for Wave-1

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,33 +1,65 @@
+import { LETTER_POOL } from './helper';
+
+// export const drawLetters = () => {
+//  // Implement this method for wave 1
+//
+//  const drawLettersList = [];
+//
+//  // Flatten the letter pool into a list
+//  for (const letter in LETTER_POOL) {
+//    const count = LETTER_POOL[letter];
+//    for (let i = 0; i < count; i++) {
+//      drawLettersList.push(letter); //.extend in Python
+//    }
+//  }
+//
+//  const resultLetters = [];
+//  let counter = 0;
+//
+//  while (counter < 10) {
+//    const index = Math.floor(Math.random() * drawLettersList.length);
+//    const letter = drawLettersList.splice(index, 1)[0]; //.pop in Python
+//    resultLetters.push(letter);
+//    counter++;
+//  }
+//
+//  return resultLetters;
+//};
+
+
+// var_2
+
 export const drawLetters = () => {
-  // Implement this method for wave 1
-  const LETTER_POOL = {
-    A: 9,B: 2,C: 2,D: 4,E: 12,F: 2,G: 3,H: 2,I: 9,J: 1,K: 1,L: 4,M: 2,
-    N: 6,O: 8,P: 2,Q: 1,R: 6,S: 4,T: 6,U: 4,V: 2,W: 2,X: 1,Y: 2,Z: 1
-};
+  const computeCumulativeWeights = (letterPool) => {
+    const cumulativeWeights = {};
+    let totalWeight = 0;
 
-  const drawLettersList = [];
-
-  // Flatten the letter pool into a list
-  for (const letter in LETTER_POOL) {
-    const count = LETTER_POOL[letter];
-    for (let i = 0; i < count; i++) {
-      drawLettersList.push(letter); //.extend in Python
+    for (const [letter, weight] of Object.entries(letterPool)) {
+      totalWeight += weight;
+      cumulativeWeights[letter] = totalWeight;
     }
-  }
+    return [cumulativeWeights, totalWeight];
+  };
 
-  const resultLetters = [];
-  let counter = 0;
+    const [cumulative, totalWeight] = computeCumulativeWeights(LETTER_POOL);
+    const selectedLetters = [];
+    const drawnCount = {}; 
 
-  while (counter < 10) {
-    const index = Math.floor(Math.random() * drawLettersList.length);
-    const letter = drawLettersList.splice(index, 1)[0]; //.pop in Python
-    resultLetters.push(letter);
-    counter++;
-  }
+  while (selectedLetters.length < 10) {
+    const randValue = Math.floor(Math.random() * totalWeight) + 1;
 
-  return resultLetters;
+    for (const [letter, cumulativeWeight] of Object.entries(cumulative)) {
+          if (randValue <= cumulativeWeight) {
+            if ((drawnCount[letter] || 0) < LETTER_POOL[letter]) {
+              drawnCount[letter] = (drawnCount[letter] || 0) + 1;
+              selectedLetters.push(letter);
+            }
+            break;
+          }
+        }
+      }
+  return selectedLetters;
 };
-
 
 export const usesAvailableLetters = (input, lettersInHand) => {
   // Implement this method for wave 2
